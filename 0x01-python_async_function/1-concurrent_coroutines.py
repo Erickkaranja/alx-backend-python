122ec1dfe71d0ab77382d3c85a897379d5e304b4
Erickkaranja <karanja99erick@gmail.com>
2023-10-09 22:47:18 +0300

    adding task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''asyncronous input/output in python'''
+import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''spawns wait_random n times with a delay of max_delay
+       args:
+           n (int): number of times to spawn wait_random.
+           max_delay (int): maximum delay of spawning wait_random.
+       Returns:
+           a list of floats generated from running wait_random.
+    '''
+    my_list = []
+    corotines = [wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(corotines):
+        result = await task
+        my_list.append(result)
+    my_list.sort()
+    return my_list
