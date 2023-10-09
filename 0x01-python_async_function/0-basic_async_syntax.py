a9a1f737acbd64b145d9f52ad059b168378311f2
Erickkaranja <karanja99erick@gmail.com>
2023-10-09 14:34:25 +0300

    task 1

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''asyncronous input/output in python.'''
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    '''coroutine that waits for a given random delay time and returns it.'''
+    x = random.uniform(0, max_delay)
+    return x
