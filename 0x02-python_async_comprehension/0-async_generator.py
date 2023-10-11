6964cc3b5e1e39fdbd51773e48def2931f3c7c79
Erickkaranja <karanja99erick@gmail.com>
2023-10-11 08:00:42 +0300

    updating task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 '''asynchronous generators and comprehension.'''
-from typing import Iterator
+from typing import AsyncGenerator
 import random
 import asyncio
 
 
-async def async_generator() -> Iterator[float]:
+async def async_generator() -> AsyncGenerator[float, None]:
     '''coroutine that loops 10 times and asynchronously sleep for 1 second.
        Returns:
             a random number between 0 and 10.
     '''
-    my_list = []
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
