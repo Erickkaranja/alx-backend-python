aed8fc68fc5b06f9776c3df03f82e18c6b45c2da
Erickkaranja <karanja99erick@gmail.com>
2023-10-15 19:47:31 +0300

    updating task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''asyncronous input output in python'''
+wait_n = __import__('1-concurrent_coroutines').wait_n
+import asyncio
+import time
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    '''measures total exection time'''
+    start_time = time.time()
+
+    await asyncio.gather([wait_n(n, max_delay) for _ in range(n)])
+    end_time = time.time()
+
+    total_time = end_time - start_time
+    average_time = total_time / n
+    return average_time
